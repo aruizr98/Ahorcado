5e766ffbbe2affbd8bbbcabf2915ac93a133c17c
aruizr98 <77283313+aruizr98@users.noreply.github.com>
2021-11-11 13:36:44 +0100

    Add files via upload

---
+++ JS/formulario.js
@@ -0,0 +1,25 @@
+window.addEventListener("load", iniciar);
+
+    function iniciar() {
+        var nombreJugador1 = document.getElementById("nombreJugador1");
+        var nombreJugador2 = document.getElementById("nombreJugador2");
+        var colorJugador1 = document.getElementById("colorJugador1");
+        var colorJugador2 = document.getElementById("colorJugador2");
+        var formulario=document.getElementById("formulario");
+        var categoria=document.getElementById("categoria");
+        var modo=document.getElementsByName("modo");
+
+        formulario.addEventListener("submit", function () {
+            localStorage.setItem("nombreJugador1", nombreJugador1.value);
+            localStorage.setItem("nombreJugador2", nombreJugador2.value);
+            localStorage.setItem("colorJugador1", colorJugador1.value);
+            localStorage.setItem("colorJugador2", colorJugador2.value);
+            localStorage.setItem("categoria", categoria.value);
+            if(modo[0].checked){
+                localStorage.setItem("modo", modo[0].value);
+            }else{
+                localStorage.setItem("modo",modo[1].value);
+            }
+            window.open("pantalla de juego.html", "Ahorcado");
+        })
+    }
