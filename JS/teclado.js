a31806b270142696f1af803d29e0282e6da90b56
aruizr98 <77283313+aruizr98@users.noreply.github.com>
2021-11-16 10:14:15 +0100

    Añadida la función cambiaTurno al pulsar teclas
    Al pulsar una letra, se resetea el cronómetro y se cambia de turno.

--- JS/teclado.js
+++ JS/teclado.js
@@ -1,31 +1,41 @@
 window.addEventListener("load", iniciar);
+
 function iniciar() {
     var teclado = document.getElementById("teclado");
     var teclas = teclado.getElementsByTagName("button");
-    var aceptarTeclado=document.getElementById("aceptarTeclado");
-    var inputTeclado=document.getElementById("inputTeclado");
-    var resolver=document.getElementById("resolver");
+    var aceptarTeclado = document.getElementById("aceptarTeclado");
+    var inputTeclado = document.getElementById("inputTeclado");
+    var resolver = document.getElementById("resolver");
     var nuevaPartida = document.getElementById("nuevaPartida");
+    var cronometro1 = document.getElementById("cronometro1");
+    var cronometro2 = document.getElementById("cronometro2");
 
     for (let index = 0; index < teclas.length; index++) {
         teclas[index].addEventListener("click", function (e) {
             sessionStorage.setItem("letraPulsada", e.target.value);
+            cambioTurno();
+            if(turno==1){
+                cronometro1.innerText="30";
+                cronometro2.innerText="30";
+            }else{
+                cronometro2.innerText="30";
+                cronometro1.innerText="30";
+            }
         })
     }
-    
-    aceptarTeclado.addEventListener("click", function(){
+
+    aceptarTeclado.addEventListener("click", function () {
         sessionStorage.setItem("letraPulsada", inputTeclado.value);
+        cambioTurno();
     })
-    resolver.addEventListener("click", function(){
-        let solucion=prompt("Introduce la palabra o frase");
+    resolver.addEventListener("click", function () {
+        let solucion = prompt("Introduce la palabra o frase");
     })
-    nuevaPartida.addEventListener("click", function(){
-       if(confirm("¿Estás seguro de que quieres empezar una nueva partida? Se borrarán todos los datos.")){
-           localStorage.clear();
-           window.open("formularioInicial.html", "formulario inicial");
-       }
+    nuevaPartida.addEventListener("click", function () {
+        if (confirm("¿Estás seguro de que quieres empezar una nueva partida? Se borrarán todos los datos.")) {
+            localStorage.clear();
+            window.open("formularioInicial.html", "formulario inicial");
+        }
 
     })
 }
-
-
