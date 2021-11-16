a2f95678364f93f86407130c1934c2d83a297063
antoniodaw1 <77328307+antoniodaw1@users.noreply.github.com>
2021-11-16 09:49:28 +0100

    Cambio Distribuicion Array
    He cambiado el array hacia arriba para que ejecute las palabras antes .

--- JS/general.js
+++ JS/general.js
@@ -1,9 +1,11 @@
-window.addEventListener("load", iniciar);
-function iniciar(){
-    var peliculas=["Batman", "Origen", "Star Wars", "Indiana Jones", "Piratas del Caribe", "Interstellar", "Origen", "Seven", "Regreso al Futuro", "Expediente Warren", "Terminator", "Toy Story", "Up", "Paranormal Activity", "El faro", "Spiderman", "Blade Runner", "V de Vendetta", "Monstruos SA", "La Vida De Brian", "Watchmen"];
+var peliculas=["Batman", "Origen", "Star Wars", "Indiana Jones", "Piratas del Caribe", "Interstellar", "Origen", "Seven", "Regreso al Futuro", "Expediente Warren", "Terminator", "Toy Story", "Up", "Paranormal Activity", "El faro", "Spiderman", "Blade Runner", "V de Vendetta", "Monstruos SA", "La Vida De Brian", "Watchmen"];
     var videojuegos=["Super Mario Bros", "The Legend of Zelda", "Metroid", "Uncharted", "God of War", "Tetris", "Profesor Layton", "Sonic The Hedgehog", "Half Life", "Spyro", "Crash Bandicoot", "Pokemon", "Metal Gear Solid", "Kirby", "Donkey Kong", "Pac Man", "Gran Turismo", "Grand Theft Auto", "Red Dead Redemption", "Call Of Duty", "Assassins Creed", "Rayman", "Splinter Cell"];
     var series=["Breaking Bad", "Prison Break", "House", "Doctor Who", "Sherlock", "Better Call Saul", "The Walking Dead", "Bojack Horseman", "The Crown", "La Maldicion De Hill House", "Stranger Things", "luther", "Jessica Jones", "Black Mirror", "Mr Robot", "You", "Los Simpson", "Futurama"];
     var grupos=["Radiohead", "Blur", "Oasis", "Led Zeppelin", "Metallica", "Daft Punk", "Gorillaz", "Guns And Roses", "The Beatles", "Portishead", "Ramones", "Nirvana", "Kiss", "The Rolling Stornes", "DragonForce", "Lynyrd Skynyrd", "Audioslave", "Coldplay", "LCD Soundsystem", "Rammstein", "Queen", "Muse"];
+
+window.addEventListener("load", iniciar);
+function iniciar(){
+    
     
     var cajaJugador1=document.getElementById("cajaJugador1");
     var cajaJugador2=document.getElementById("cajaJugador2");
@@ -17,6 +19,8 @@ function iniciar(){
     nombreJugador1.innerText=localStorage.getItem("nombreJugador1");
     nombreJugador2.innerText=localStorage.getItem("nombreJugador2");
     categoria.innerText=localStorage.getItem("categoria");
+
+
 
     
 }
