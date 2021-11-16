54bbf48617b2da9518b11270bf15f44127ab84b3
aruizr98 <77283313+aruizr98@users.noreply.github.com>
2021-11-16 10:51:42 +0100

    Corregida la posición de la palabra.
    He cambiado el sitio en el que aparece la palabra, ya que antes estaba muy arriba porque se hacía un innerHTML en el div, en lugar de en el h1.

--- JS/ocultarPalabras.js
+++ JS/ocultarPalabras.js
@@ -4,16 +4,16 @@ function iniciar(){
    
 
     if (localStorage.getItem("categoria")=== "peliculas") {
-        document.getElementById("palabra").innerHTML=peliculas[Math.floor(peliculas.length*Math.random())];
+        document.getElementById("palabra").children[0].innerHTML=peliculas[Math.floor(peliculas.length*Math.random())];
     }
     else if(localStorage.getItem("categoria")=== "videojuegos") {
-        document.getElementById("palabra").innerHTML=videojuegos[Math.floor(videojuegos.length*Math.random())];
+        document.getElementById("palabra").children[0].innerHTML=videojuegos[Math.floor(videojuegos.length*Math.random())];
     }
     else if(localStorage.getItem("categoria")=== "series") {
-        document.getElementById("palabra").innerHTML=series[Math.floor(series.length*Math.random())];
+        document.getElementById("palabra").children[0].innerHTML=series[Math.floor(series.length*Math.random())];
     }
     else if(localStorage.getItem("categoria")=== "grupos") {
-        document.getElementById("palabra").innerHTML=grupos[Math.floor(grupos.length*Math.random())];
+        document.getElementById("palabra").children[0].innerHTML=grupos[Math.floor(grupos.length*Math.random())];
     }
     
 
