f9447368d6086fcdee65d8896f6966f0d0e47a9b
antoniodaw1 <77328307+antoniodaw1@users.noreply.github.com>
2021-11-17 10:24:39 +0100

    Aqui ya funciona la palabra oculta
    Se muestran las rayas de la palabra oculta , pero no resuelve y sale un error si intentas poner una letra

--- JS/ocultarPalabras.js
+++ JS/ocultarPalabras.js
@@ -1,21 +1,19 @@
+window.addEventListener("load",iniciar);
+    function iniciar(){
 
-window.addEventListener("load", iniciar);
-function iniciar(){
-   
+
     function cogerPalabraAleatoria(){
         let palabraAleatoria;
 
         if (localStorage.getItem("categoria")=== "peliculas") {
             
             palabraAleatoria= peliculas[Math.floor(peliculas.length*Math.random())]
-            // document.getElementById("palabra").children[0].innerHTML=palabraAleatoria;
 
         }
 
         else if(localStorage.getItem("categoria")=== "videojuegos") {
             palabraAleatoria= videojuegos[Math.floor(videojuegos.length*Math.random())]
 
-    
         }
         else if(localStorage.getItem("categoria")=== "series") {
             palabraAleatoria= series[Math.floor(series.length*Math.random())]
@@ -27,19 +25,30 @@ function iniciar(){
        return palabraAleatoria;
 
     }
+
+
     let palabra=cogerPalabraAleatoria();
-    // console.log(palabra.split(""));
+    console.log(palabra);
+    
+
     let palabraArray = palabra.split("");
-    // console.log(palabraArray.length)
 
     function palabraOculta(palabraArray){
-        for (let i = 0; i < palabraArray.length; i++) {
-            console.log(palabraArray.length)
-        }
         
+        for (let i = 0; i < palabraArray.length; i++) {
+            if (palabraArray[i]!=" ") {
+                palabraArray[i]="_";
+            }
+            else{
+                palabraArray[i]="/";
+            }
 
+        }
+        return palabraArray.join(" ");        
+ 
 
     }
     
-    palabraOculta(palabraArray);
-}
+    document.getElementById("palabra").children[0].innerHTML=palabraOculta(palabraArray);
+
+    }
