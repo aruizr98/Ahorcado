9f63e8ace20a13681a445bbffeb61d49fb49fafb
aruizr98 <77283313+aruizr98@users.noreply.github.com>
2021-11-25 13:56:08 +0100

    Modificado el botón de resolver
    Ahora funciona y te pide que introduzcas la palabra. Si no es correcta, se avisa al jugador, y si es correcta, se abre una ventana con el resúmen de la partida. OJO: DE VEZ EN CUANDO (Y APARENTEMENTE ALEATORIAMENTE) SE PRODUCE UN ERRROR POR CONSOLA QUE HAY QUE REVISAR.

--- JS/teclado.js
+++ JS/teclado.js
@@ -35,7 +35,37 @@ function iniciar() {
     })
     resolver.addEventListener("click", function () {
         let solucion = prompt("Introduce la palabra o frase");
+        let victoriasJugador1=localStorage.getItem("victoriasJugador1");
+        let victoriasJugador2=localStorage.getItem("victoriasJugador2");
+        if(solucion==palabra){
+            alert("Enhorabuena, has ganado.");
+            if(turno==1){
+                victoriasJugador1++;
+                localStorage.setItem("victoriasJugador1",victoriasJugador1++);
+                finDeJuego();
+            }else{  
+                victoriasJugador2++;
+                localStorage.setItem("victoriasJugador2", victoriasJugador2++);
+                finDeJuego();
+            }
+        }else{
+            alert("Incorrecto");
+        }
     })
+    function finDeJuego(){
+        let nombre1=localStorage.getItem("nombreJugador1");
+        let nombre2=localStorage.getItem("nombreJugador2");
+        var ventana=window.open("about:blank", "fin de juego", "width=800 height=600");
+        ventana.document.write("<h1>Resúmen de la partida</h1>");
+        if(turno==1){
+            ventana.document.write("<h2>Ganador: "+nombre1+"</h2>");
+        }else{
+            ventana.document.write("<h2>Ganador: "+nombre2+"</h2>");
+        }
+        ventana.document.write("<h2>Victorias jugador 1("+nombre1+"): "+localStorage.getItem("victoriasJugador1")+"</h2>");
+        ventana.document.write("<h2>Victorias jugador 2("+nombre2+"): "+localStorage.getItem("victoriasJugador2")+"</h2>");
+        
+    }
     nuevaPartida.addEventListener("click", function () {
         if (confirm("¿Estás seguro de que quieres empezar una nueva partida? Se borrarán todos los datos.")) {
             localStorage.clear();
