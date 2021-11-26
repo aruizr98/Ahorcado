b18c1f6e392c013c54fa2b58bf90a880cf2e19e3
aruizr98 <77283313+aruizr98@users.noreply.github.com>
2021-11-26 10:36:48 +0100

    Modificado el efecto de las letras.
    Ahora al pulsar una letra por primera vez, se le baja la opacidad, se elimina el efecto hover y se le cambia el cursor a la flecha. OJO:  Esto es solo visible. Faltaría cambiar el funcionamiento real para que no se pueda pulsar una letra por segunda vez.

--- JS/teclado.js
+++ JS/teclado.js
@@ -16,78 +16,81 @@ function iniciar() {
             cambioTurno();
             cronometro1.innerText = "30";
             cronometro2.innerText = "30";
-        document.getElementById("palabra").children[0].innerHTML=comprobarLetra(sessionStorage.getItem("letraPulsada"));
-            
+            document.getElementById("palabra").children[0].innerHTML = comprobarLetra(sessionStorage.getItem("letraPulsada"));
+            teclas[index].style.opacity="0.2";
+            teclas[index].classList="";
+            teclas[index].style.cursor="unset";
         })
 
     }
 
     aceptarTeclado.addEventListener("click", function () {
         sessionStorage.setItem("letraPulsada", inputTeclado.value);
-        inputTeclado.value="";  
+        inputTeclado.value = "";
         cambioTurno();
         cronometro1.innerText = "30";
         cronometro2.innerText = "30";
         cambioTurno();
-        document.getElementById("palabra").children[0].innerHTML=comprobarLetra(sessionStorage.getItem("letraPulsada"));
-        
+        document.getElementById("palabra").children[0].innerHTML = comprobarLetra(sessionStorage.getItem("letraPulsada"));
+
 
     })
     resolver.addEventListener("click", function () {
         let solucion = prompt("Introduce la palabra o frase");
-        let victoriasJugador1=localStorage.getItem("victoriasJugador1");
-        let victoriasJugador2=localStorage.getItem("victoriasJugador2");
-        if(solucion==palabra){
+        let victoriasJugador1 = localStorage.getItem("victoriasJugador1");
+        let victoriasJugador2 = localStorage.getItem("victoriasJugador2");
+        if (solucion == palabra) {
             alert("Enhorabuena, has ganado.");
-            if(turno==1){
+            if (turno == 1) {
                 victoriasJugador1++;
-                localStorage.setItem("victoriasJugador1",victoriasJugador1++);
+                localStorage.setItem("victoriasJugador1", victoriasJugador1++);
                 finDeJuego();
-            }else{  
+            } else {
                 victoriasJugador2++;
                 localStorage.setItem("victoriasJugador2", victoriasJugador2++);
                 finDeJuego();
             }
-        }else{
+        } else {
             alert("Incorrecto");
             cambioTurno();
         }
     })
-    function finDeJuego(){
-        let nombre1=localStorage.getItem("nombreJugador1");
-        let nombre2=localStorage.getItem("nombreJugador2");
-        var pantallaResumen=document.getElementById("resumen");
-        pantallaResumen.style.display="";
-        let h1=document.createElement("h1");
+
+    function finDeJuego() {
+        let nombre1 = localStorage.getItem("nombreJugador1");
+        let nombre2 = localStorage.getItem("nombreJugador2");
+        var pantallaResumen = document.getElementById("resumen");
+        pantallaResumen.style.display = "";
+        let h1 = document.createElement("h1");
         h1.append("Resúmen de la partida");
         pantallaResumen.append(h1);
-        let ganador=document.createElement("h2");
-        
-        if(turno==1){
-            ganador.append("Ganador: "+nombre1);
+        let ganador = document.createElement("h2");
+
+        if (turno == 1) {
+            ganador.append("Ganador: " + nombre1);
             pantallaResumen.append(ganador);
-        }else{
-            ganador.append("Ganador: "+nombre2);
+        } else {
+            ganador.append("Ganador: " + nombre2);
             pantallaResumen.append(ganador);
         }
-        let victorias1=document.createElement("h2");
-        let victorias2=document.createElement("h2");
-        victorias1.append("Victorias jugador 1 ("+nombre1+"): "+localStorage.getItem("victoriasJugador1"));
+        let victorias1 = document.createElement("h2");
+        let victorias2 = document.createElement("h2");
+        victorias1.append("Victorias jugador 1 (" + nombre1 + "): " + localStorage.getItem("victoriasJugador1"));
         pantallaResumen.append(victorias1);
-        victorias2.append("Victorias jugador 2 ("+nombre2+"): "+localStorage.getItem("victoriasJugador2"));
+        victorias2.append("Victorias jugador 2 (" + nombre2 + "): " + localStorage.getItem("victoriasJugador2"));
         pantallaResumen.append(victorias2);
-        let volverAJugar=document.createElement("button");
-        let volverAlFormulario=document.createElement("button");
-        volverAJugar.innerText="Volver a jugar";
+        let volverAJugar = document.createElement("button");
+        let volverAlFormulario = document.createElement("button");
+        volverAJugar.innerText = "Volver a jugar";
         volverAJugar.setAttribute("onclick", "location.reload()");
-        volverAJugar.setAttribute("style","float:left; margin-left:20px; background-color:black; color:white; cursor:pointer;");
+        volverAJugar.setAttribute("style", "float:left; margin-left:20px; background-color:black; color:white; cursor:pointer;");
         pantallaResumen.append(volverAJugar);
 
-        volverAlFormulario.innerText="Volver al formulario inicial";
+        volverAlFormulario.innerText = "Volver al formulario inicial";
         volverAlFormulario.setAttribute("onclick", "location.href='FormularioInicial.html'");
-        volverAlFormulario.setAttribute("style","float:right; margin-right:20px; background-color:black; color:white; cursor:pointer;");
+        volverAlFormulario.setAttribute("style", "float:right; margin-right:20px; background-color:black; color:white; cursor:pointer;");
         pantallaResumen.append(volverAlFormulario);
-       
+
     }
     nuevaPartida.addEventListener("click", function () {
         if (confirm("¿Estás seguro de que quieres empezar una nueva partida? Se borrarán todos los datos.")) {
