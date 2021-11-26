a74d60403675384a29ba7cbe38c1d55c1924ea11
aruizr98 <77283313+aruizr98@users.noreply.github.com>
2021-11-26 10:12:50 +0100

    Adaptado el botón resolver para el div.
    Ahora el botón resolver muestra un div en lugar de una nueva ventana.

--- JS/teclado.js
+++ JS/teclado.js
@@ -56,15 +56,37 @@ function iniciar() {
     function finDeJuego(){
         let nombre1=localStorage.getItem("nombreJugador1");
         let nombre2=localStorage.getItem("nombreJugador2");
-        var ventana=window.open("about:blank", "fin de juego", "width=800 height=600");
-        ventana.document.write("<h1>Resúmen de la partida</h1>");
+        var pantallaResumen=document.getElementById("resumen");
+        pantallaResumen.style.display="";
+        let h1=document.createElement("h1");
+        h1.append("Resúmen de la partida");
+        pantallaResumen.append(h1);
+        let ganador=document.createElement("h2");
+        
         if(turno==1){
-            ventana.document.write("<h2>Ganador: "+nombre1+"</h2>");
+            ganador.append("Ganador: "+nombre1);
+            pantallaResumen.append(ganador);
         }else{
-            ventana.document.write("<h2>Ganador: "+nombre2+"</h2>");
+            ganador.append("Ganador: "+nombre2);
+            pantallaResumen.append(ganador);
         }
-        ventana.document.write("<h2>Victorias jugador 1("+nombre1+"): "+localStorage.getItem("victoriasJugador1")+"</h2>");
-        ventana.document.write("<h2>Victorias jugador 2("+nombre2+"): "+localStorage.getItem("victoriasJugador2")+"</h2>");
+        let victorias1=document.createElement("h2");
+        let victorias2=document.createElement("h2");
+        victorias1.append("Victorias jugador 1 ("+nombre1+"): "+localStorage.getItem("victoriasJugador1"));
+        pantallaResumen.append(victorias1);
+        victorias2.append("Victorias jugador 2 ("+nombre2+"): "+localStorage.getItem("victoriasJugador2"));
+        pantallaResumen.append(victorias2);
+        let volverAJugar=document.createElement("button");
+        let volverAlFormulario=document.createElement("button");
+        volverAJugar.innerText="Volver a jugar";
+        volverAJugar.setAttribute("onclick", "location.reload()");
+        volverAJugar.setAttribute("style","float:left; margin-left:20px; background-color:black; color:white; cursor:pointer;");
+        pantallaResumen.append(volverAJugar);
+
+        volverAlFormulario.innerText="Volver al formulario inicial";
+        volverAlFormulario.setAttribute("onclick", "location.href='FormularioInicial.html'");
+        volverAlFormulario.setAttribute("style","float:right; margin-right:20px; background-color:black; color:white; cursor:pointer;");
+        pantallaResumen.append(volverAlFormulario);
        
     }
     nuevaPartida.addEventListener("click", function () {
