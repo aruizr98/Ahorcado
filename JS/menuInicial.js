d7359430c844fc1d664186dda1ee560b676f96a0
aruizr98 <77283313+aruizr98@users.noreply.github.com>
2021-11-30 16:45:06 +0100

    Añadidos los datos al ranking
    Ahora se muestran las victorias de los jugadores en orden en el ranking.

--- JS/menuInicial.js
+++ JS/menuInicial.js
@@ -2,6 +2,20 @@ window.addEventListener("load", iniciar);
 function iniciar() {
     document.getElementById("muestraOculta").addEventListener("click", mostrarReglas);
     document.getElementById("cerrarReglas").addEventListener("click", cerrarReglas);
+    var tabla=document.getElementById("puntuaciones");
+    if(localStorage.getItem("victoriasJugador1")>0){
+        //tabla.innerText=localStorage.getItem("nombreJugador1")+": "+localStorage.getItem("victoriasJugador1");
+        comprobarMejorJugador();
+    }
+    function comprobarMejorJugador(){
+        if((localStorage.getItem("victoriasJugador1") > localStorage.getItem("victoriasJugador2") || localStorage.getItem("victoriasJugador1") == localStorage.getItem("victoriasJugador2"))){
+            tabla.innerHTML="<li>"+localStorage.getItem("nombreJugador1")+": "+localStorage.getItem("victoriasJugador1")+"</li>";
+            tabla.innerHTML+="<li>"+localStorage.getItem("nombreJugador2")+": "+localStorage.getItem("victoriasJugador2")+"</li>";
+        }else if(localStorage.getItem("victoriasJugador2") > localStorage.getItem("victoriasJugador1")){
+            tabla.innerHTML="<li>"+localStorage.getItem("nombreJugador2")+": "+localStorage.getItem("victoriasJugador2")+"</li>";
+            tabla.innerHTML+="<li>"+localStorage.getItem("nombreJugador1")+": "+localStorage.getItem("victoriasJugador1")+"</li>";
+        }
+    }
 }
 
 function mostrarReglas() {
@@ -13,3 +27,4 @@ function cerrarReglas() {
     document.getElementById("reglas").style.display = "none";
     document.getElementById("sombraFondoReglas").style.display = "none";
 }
+
