f2960081af54902ab01e9d4789c9332c097d8784
aruizr98 <77283313+aruizr98@users.noreply.github.com>
2021-11-30 16:47:44 +0100

    Corregido pequeño error
    He quitado un if que tenía para llamar a la función de comprobarMejorJugador porque ya no era necesaria, ya que los condicionales están dentro de la función.

--- JS/menuInicial.js
+++ JS/menuInicial.js
@@ -2,18 +2,16 @@ window.addEventListener("load", iniciar);
 function iniciar() {
     document.getElementById("muestraOculta").addEventListener("click", mostrarReglas);
     document.getElementById("cerrarReglas").addEventListener("click", cerrarReglas);
-    var tabla=document.getElementById("puntuaciones");
-    if(localStorage.getItem("victoriasJugador1")>0){
-        //tabla.innerText=localStorage.getItem("nombreJugador1")+": "+localStorage.getItem("victoriasJugador1");
-        comprobarMejorJugador();
-    }
-    function comprobarMejorJugador(){
-        if((localStorage.getItem("victoriasJugador1") > localStorage.getItem("victoriasJugador2") || localStorage.getItem("victoriasJugador1") == localStorage.getItem("victoriasJugador2"))){
-            tabla.innerHTML="<li>"+localStorage.getItem("nombreJugador1")+": "+localStorage.getItem("victoriasJugador1")+"</li>";
-            tabla.innerHTML+="<li>"+localStorage.getItem("nombreJugador2")+": "+localStorage.getItem("victoriasJugador2")+"</li>";
-        }else if(localStorage.getItem("victoriasJugador2") > localStorage.getItem("victoriasJugador1")){
-            tabla.innerHTML="<li>"+localStorage.getItem("nombreJugador2")+": "+localStorage.getItem("victoriasJugador2")+"</li>";
-            tabla.innerHTML+="<li>"+localStorage.getItem("nombreJugador1")+": "+localStorage.getItem("victoriasJugador1")+"</li>";
+    var tabla = document.getElementById("puntuaciones");
+    comprobarMejorJugador();
+
+    function comprobarMejorJugador() {
+        if ((localStorage.getItem("victoriasJugador1") > localStorage.getItem("victoriasJugador2") || localStorage.getItem("victoriasJugador1") == localStorage.getItem("victoriasJugador2"))) {
+            tabla.innerHTML = "<li>" + localStorage.getItem("nombreJugador1") + ": " + localStorage.getItem("victoriasJugador1") + "</li>";
+            tabla.innerHTML += "<li>" + localStorage.getItem("nombreJugador2") + ": " + localStorage.getItem("victoriasJugador2") + "</li>";
+        } else if (localStorage.getItem("victoriasJugador2") > localStorage.getItem("victoriasJugador1")) {
+            tabla.innerHTML = "<li>" + localStorage.getItem("nombreJugador2") + ": " + localStorage.getItem("victoriasJugador2") + "</li>";
+            tabla.innerHTML += "<li>" + localStorage.getItem("nombreJugador1") + ": " + localStorage.getItem("victoriasJugador1") + "</li>";
         }
     }
 }
