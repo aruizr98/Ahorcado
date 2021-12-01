bd163ceb5a4c8bea890d5f5c5dfa2043ea1a6db6
aruizr98 <77283313+aruizr98@users.noreply.github.com>
2021-12-01 15:59:57 +0100

    Actualizado para el funcionamiento del ranking
    He añadido un condicional para que solo se reseteen las victorias de los jugadores si el usuario ha dicho quie quiere mantener los datos.

--- JS/formulario.js
+++ JS/formulario.js
@@ -1,33 +1,34 @@
 window.addEventListener("load", iniciar);
 
-    function iniciar() {
-        
-        var nombreJugador1 = document.getElementById("nombreJugador1");
-        var nombreJugador2 = document.getElementById("nombreJugador2");
-        var colorJugador1 = document.getElementById("colorJugador1");
-        var colorJugador2 = document.getElementById("colorJugador2");
-        var formulario=document.getElementById("formulario");
-        var categoria=document.getElementById("categoria");
-        var modo=document.getElementsByName("modo");
+function iniciar() {
+    var nombreJugador1 = document.getElementById("nombreJugador1");
+    var nombreJugador2 = document.getElementById("nombreJugador2");
+    var colorJugador1 = document.getElementById("colorJugador1");
+    var colorJugador2 = document.getElementById("colorJugador2");
+    var formulario = document.getElementById("formulario");
+    var categoria = document.getElementById("categoria");
+    var modo = document.getElementsByName("modo");
 
-        formulario.addEventListener("submit", function () {
-            
-            sessionStorage.setItem("contador", 0);
-            localStorage.setItem("nombreJugador1", nombreJugador1.value);
-            localStorage.setItem("nombreJugador2", nombreJugador2.value);
-            localStorage.setItem("colorJugador1", colorJugador1.value);
-            localStorage.setItem("colorJugador2", colorJugador2.value);
-            localStorage.setItem("categoria", categoria.value);
+    formulario.addEventListener("submit", function () {
+        sessionStorage.setItem("contador", 0);
+        localStorage.setItem("nombreJugador1", nombreJugador1.value);
+        localStorage.setItem("nombreJugador2", nombreJugador2.value);
+        localStorage.setItem("colorJugador1", colorJugador1.value);
+        localStorage.setItem("colorJugador2", colorJugador2.value);
+        localStorage.setItem("categoria", categoria.value);
+        if (sessionStorage.getItem("conservarCambios")=="false") {
             localStorage.setItem("victoriasJugador1", 0);
-            localStorage.setItem("victoriasJugador2",0);
-            sessionStorage.setItem("fallosJugador1", 0);
-            sessionStorage.setItem("fallosJugador2", 0);
-            if(modo[0].checked){
-                localStorage.setItem("modo", modo[0].value);
-            }else{
-                localStorage.setItem("modo",modo[1].value);
-            }
-            window.open("pantalla de juego.html", "Ahorcado");
-            window.close();
-        })
-    }
+            localStorage.setItem("victoriasJugador2", 0);
+        }
+        sessionStorage.setItem("fallosJugador1", 0);
+        sessionStorage.setItem("fallosJugador2", 0);
+        if (modo[0].checked) {
+            localStorage.setItem("modo", modo[0].value);
+        } else {
+            localStorage.setItem("modo", modo[1].value);
+        }
+        window.open("pantalla de juego.html", "Ahorcado");
+        this.close();
+
+    })
+}
