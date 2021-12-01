0c334181ef212eb1a902dce54c153df59569cca2
aruizr98 <77283313+aruizr98@users.noreply.github.com>
2021-12-01 15:58:45 +0100

    Actualizado el funcionamiento del ranking.
    Ahora, si hay datos en el ranking, al pulsar el botón para comenzar a jugar, te pregunta si quieres conservar esos datos del ranking o si quieres empezar de 0.

--- JS/menuInicial.js
+++ JS/menuInicial.js
@@ -1,12 +1,14 @@
 window.addEventListener("load", iniciar);
 function iniciar() {
+    var botonIniciar = document.getElementById("comenzarJuego");
     document.getElementById("muestraOculta").addEventListener("click", mostrarReglas);
     document.getElementById("cerrarReglas").addEventListener("click", cerrarReglas);
-
     var tabla = document.getElementById("puntuaciones");
     comprobarMejorJugador();
+
     function comprobarMejorJugador() {
-        if ((localStorage.getItem("victoriasJugador1") > localStorage.getItem("victoriasJugador2") || localStorage.getItem("victoriasJugador1") == localStorage.getItem("victoriasJugador2"))) {
+        if(localStorage.getItem("victoriasJugador1") > 0 || localStorage.getItem("victoriasJugador2") > 0){
+        if ((localStorage.getItem("victoriasJugador1") > localStorage.getItem("victoriasJugador2")) || (localStorage.getItem("victoriasJugador1")==localStorage.getItem("victoriasJugador2"))) {
             tabla.innerHTML = "<li>" + localStorage.getItem("nombreJugador1") + ": " + localStorage.getItem("victoriasJugador1") + "</li>";
             tabla.innerHTML += "<li>" + localStorage.getItem("nombreJugador2") + ": " + localStorage.getItem("victoriasJugador2") + "</li>";
         } else if (localStorage.getItem("victoriasJugador2") > localStorage.getItem("victoriasJugador1")) {
@@ -14,13 +16,13 @@ function iniciar() {
             tabla.innerHTML += "<li>" + localStorage.getItem("nombreJugador1") + ": " + localStorage.getItem("victoriasJugador1") + "</li>";
         }
     }
-
-    document.getElementById("contenedor").addEventListener("click", playMusicaFondo);
-    function playMusicaFondo() {
-
-        document.getElementById("musicaFondo").volume = 0.25;
-        document.getElementById("musicaFondo").play();
     }
+    botonIniciar.addEventListener("click", function () {
+        if (localStorage.getItem("victoriasJugador1") >0 || localStorage.getItem("victoriasJugador2") >0) {
+            let respuesta=confirm("Hay datos de juego anteriores. ¿Quieres conservarlos?");
+            sessionStorage.setItem("conservarCambios", respuesta);
+        }
+    })
 }
 
 function mostrarReglas() {
