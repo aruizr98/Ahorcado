31e39925ab554c59811d85c1bafa92ad5380da6e
JuanCarlosRobles88 <77584078+JuanCarlosRobles88@users.noreply.github.com>
2021-12-01 12:29:24 +0100

    para que funcione el audio
    me cago en google que no permite el autoplay

--- JS/menuInicial.js
+++ JS/menuInicial.js
@@ -2,17 +2,24 @@ window.addEventListener("load", iniciar);
 function iniciar() {
     document.getElementById("muestraOculta").addEventListener("click", mostrarReglas);
     document.getElementById("cerrarReglas").addEventListener("click", cerrarReglas);
+
     var tabla = document.getElementById("puntuaciones");
     comprobarMejorJugador();
-
     function comprobarMejorJugador() {
-        if ((localStorage.getItem("victoriasJugador1") > localStorage.getItem("victoriasJugador2") )) {
+        if ((localStorage.getItem("victoriasJugador1") > localStorage.getItem("victoriasJugador2") || localStorage.getItem("victoriasJugador1") == localStorage.getItem("victoriasJugador2"))) {
             tabla.innerHTML = "<li>" + localStorage.getItem("nombreJugador1") + ": " + localStorage.getItem("victoriasJugador1") + "</li>";
             tabla.innerHTML += "<li>" + localStorage.getItem("nombreJugador2") + ": " + localStorage.getItem("victoriasJugador2") + "</li>";
         } else if (localStorage.getItem("victoriasJugador2") > localStorage.getItem("victoriasJugador1")) {
             tabla.innerHTML = "<li>" + localStorage.getItem("nombreJugador2") + ": " + localStorage.getItem("victoriasJugador2") + "</li>";
             tabla.innerHTML += "<li>" + localStorage.getItem("nombreJugador1") + ": " + localStorage.getItem("victoriasJugador1") + "</li>";
         }
+    }
+
+    document.getElementById("contenedor").addEventListener("click", playMusicaFondo);
+    function playMusicaFondo() {
+
+        document.getElementById("musicaFondo").volume = 0.25;
+        document.getElementById("musicaFondo").play();
     }
 }
 
