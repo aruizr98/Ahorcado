083515ba4f79f2c1685615216631a9576055664c
aruizr98 <77283313+aruizr98@users.noreply.github.com>
2021-12-02 09:05:44 +0100

    Actualizado para guardar partida
    Si el usuario quiere conservar los cambios, se le redirige a la pantalla de juego con la palabra, y victorias que tenía antes.

--- JS/menuInicial.js
+++ JS/menuInicial.js
@@ -1,4 +1,5 @@
 window.addEventListener("load", iniciar);
+
 function iniciar() {
     var botonIniciar = document.getElementById("comenzarJuego");
     document.getElementById("muestraOculta").addEventListener("click", mostrarReglas);
@@ -7,20 +8,27 @@ function iniciar() {
     comprobarMejorJugador();
 
     function comprobarMejorJugador() {
-        if(localStorage.getItem("victoriasJugador1") > 0 || localStorage.getItem("victoriasJugador2") > 0){
-        if ((localStorage.getItem("victoriasJugador1") > localStorage.getItem("victoriasJugador2")) || (localStorage.getItem("victoriasJugador1")==localStorage.getItem("victoriasJugador2"))) {
-            tabla.innerHTML = "<li>" + localStorage.getItem("nombreJugador1") + ": " + localStorage.getItem("victoriasJugador1") + "</li>";
-            tabla.innerHTML += "<li>" + localStorage.getItem("nombreJugador2") + ": " + localStorage.getItem("victoriasJugador2") + "</li>";
-        } else if (localStorage.getItem("victoriasJugador2") > localStorage.getItem("victoriasJugador1")) {
-            tabla.innerHTML = "<li>" + localStorage.getItem("nombreJugador2") + ": " + localStorage.getItem("victoriasJugador2") + "</li>";
-            tabla.innerHTML += "<li>" + localStorage.getItem("nombreJugador1") + ": " + localStorage.getItem("victoriasJugador1") + "</li>";
+        if (localStorage.getItem("victoriasJugador1") > 0 || localStorage.getItem("victoriasJugador2") > 0) {
+            if ((localStorage.getItem("victoriasJugador1") > localStorage.getItem("victoriasJugador2")) || (localStorage.getItem("victoriasJugador1") == localStorage.getItem("victoriasJugador2"))) {
+                tabla.innerHTML = "<li>" + localStorage.getItem("nombreJugador1") + ": " + localStorage.getItem("victoriasJugador1") + "</li>";
+                tabla.innerHTML += "<li>" + localStorage.getItem("nombreJugador2") + ": " + localStorage.getItem("victoriasJugador2") + "</li>";
+            } else if (localStorage.getItem("victoriasJugador2") > localStorage.getItem("victoriasJugador1")) {
+                tabla.innerHTML = "<li>" + localStorage.getItem("nombreJugador2") + ": " + localStorage.getItem("victoriasJugador2") + "</li>";
+                tabla.innerHTML += "<li>" + localStorage.getItem("nombreJugador1") + ": " + localStorage.getItem("victoriasJugador1") + "</li>";
+            }
         }
     }
-    }
     botonIniciar.addEventListener("click", function () {
-        if (localStorage.getItem("victoriasJugador1") >0 || localStorage.getItem("victoriasJugador2") >0) {
-            let respuesta=confirm("Hay datos de juego anteriores. ¿Quieres conservarlos?");
+        if (localStorage.getItem("victoriasJugador1") > 0 || localStorage.getItem("victoriasJugador2") > 0) {
+            let respuesta = confirm("Hay datos de juego anteriores. ¿Quieres conservarlos?");
             sessionStorage.setItem("conservarCambios", respuesta);
+            if(respuesta){
+            sessionStorage.setItem("fallosJugador1", 0);
+            sessionStorage.setItem("fallosJugador2", 0);
+            botonIniciar.setAttribute("href", "Pantalla de juego.html");
+            }
+        } else {
+            sessionStorage.setItem("conservarCambios", false);
         }
     })
 }
@@ -34,4 +42,3 @@ function cerrarReglas() {
     document.getElementById("reglas").style.display = "none";
     document.getElementById("sombraFondoReglas").style.display = "none";
 }
-
